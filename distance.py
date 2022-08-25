27cf4d5a10b97f536b649a3bbcca3ff3ce4182f3
Rikia Stewart <rikiastewart.rs@gmail.com>
2022-08-25 15:58:54 -0400

    Haversine Distance

--- distance.py
+++ distance.py
@@ -1,17 +1,44 @@
 # handle a POST request
-from flask import Flask, request, jsonify
+from math import radians, cos, sin, asin, sqrt
+from flask import Flask
+import json
 app = Flask(__name__)
 
+data = [{
+    "lat1": 53.32055555555556,
+    "lat2": 53.31861111111111,
+    "lon1": -1.7297222222222221,
+    "lon2": -1.6997222222222223
+}]
 
-@app.route('/tests/endpoint', methods=['POST'])
-def my_test_endpoint():
-    input_json = request.get_json(force=True)
-    # force=True, above, is necessary if another developer
-    # forgot to set the MIME type to 'application/json'
-    print('data from client:', input_json)
-    dictToReturn = {'answer': 42}
-    return jsonify(dictToReturn)
+coordinates = json.dumps(data)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/test", methods=["POST"])
+def distance(json_value):
+    lst = json.loads(json_value)
+
+    lat1 = lst[0]["lat1"]
+    lat2 = lst[0]["lat2"]
+    lon1 = lst[0]["lon1"]
+    lon2 = lst[0]["lon2"]
+
+    lon1 = radians(lon1)
+    lon2 = radians(lon2)
+    lat1 = radians(lat1)
+    lat2 = radians(lat2)
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+
+    c = 2 * asin(sqrt(a))
+
+    r = 6371
+
+    return (c * r)
+
+
+newList = distance(coordinates)
+print(coordinates)
+print(newList, 'K.M')
