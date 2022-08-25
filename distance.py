28fc42926d7459a13f5d62e5c6d6c02f872b2656
Rikia Stewart <rikiastewart.rs@gmail.com>
2022-08-25 11:44:00 -0400

    Flask applications

---
+++ distance.py
@@ -0,0 +1,17 @@
+# handle a POST request
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+
+@app.route('/tests/endpoint', methods=['POST'])
+def my_test_endpoint():
+    input_json = request.get_json(force=True)
+    # force=True, above, is necessary if another developer
+    # forgot to set the MIME type to 'application/json'
+    print('data from client:', input_json)
+    dictToReturn = {'answer': 42}
+    return jsonify(dictToReturn)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
