59da4a01463874499db360ee74188771c494d7c6
Rikia Stewart <rikiastewart.rs@gmail.com>
2022-08-28 22:37:50 -0400

    user application

--- practice.py
+++ practice.py
@@ -1,15 +1,22 @@
 
 
-values = [10, 11, 43, 453, 5, 4, 5, 66, 28]
+values = [10, 11, 43, -453, 5, 4, 5, "12", 28]
 
 
 def larget_number(val):
+
     return max(val)
 
 
+print("larget_number", larget_number(values))
+
+
 def find_max_number(val):
-    max = val[0]
+    largest = val[0]
     for number in val:
-        if number > max:
-            max = number
-    return max
+        if number > largest:
+            largest = number
+    return largest
+
+
+print("find_max_number", find_max_number(values))
---
+++ user.py
@@ -0,0 +1,36 @@
+from flask import Flask
+app = Flask(__name__)
+
+# create python dictionary to hold user data
+user_account = {
+    1:
+        {"first_name": 'betty',
+         "last_name": 'joy',
+         "phone_number": '0493827405',
+         "email_address": 'bettyjoy@accounts.com'
+         }
+}
+
+current_user = 1
+
+
+@app.route('/user/<id>')
+def get_user(id):
+    return user_account[id]
+
+
+@app.route('/user/<id>', methods=["POST"])
+def create_user():
+    current_user += 1
+    return
+
+
+@app.route('/user/<id>', methods=["PUT"])
+def update_user():
+    return
+
+
+@app.route('/user/<id>', methods=["DELETE"])
+def delete_user():
+    current_user -= 1
+    return
